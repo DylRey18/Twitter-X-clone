9da4b2c315d27a2985fe6b7861ad46c063bd1383
DylRey18 <111478803+DylRey18@users.noreply.github.com>
2025-04-18 16:27:39 +0700

    sidebar progress

---
+++ Frontend/twitter-clone/src/components/Sidebar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./Sidebar.css"
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      {/*x icon */}
+
+      {/*home*/}
+
+      {/*explore*/}
+
+      {/*notification*/}
+
+      {/*message*/}
+
+      {/*bookmark*/}
+
+      {/*Profile*/}
+      
+    </div>   
+
+  )
+}
+
+export default Sidebar
