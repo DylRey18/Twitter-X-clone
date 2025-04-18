70cb57ddc9806928d3819ab88077387495246f0d
DylRey18 <111478803+DylRey18@users.noreply.github.com>
2025-04-18 16:53:36 +0700

    added icon

--- Frontend/twitter-clone/src/components/Sidebar.js
+++ Frontend/twitter-clone/src/components/Sidebar.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import "./Sidebar.css"
+//import "./Sidebar.css"
+import { FaXTwitter } from "react-icons/fa6";
 
 function Sidebar() {
   return (
     <div className='sidebar'>
       {/*x icon */}
-
+        <FaXTwitter />
       {/*home*/}
-
+    
       {/*explore*/}
 
       {/*notification*/}
