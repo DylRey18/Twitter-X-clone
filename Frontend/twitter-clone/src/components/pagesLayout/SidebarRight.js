f017e20a5f0fa14351c6d15d54ceddba01ebeb5f
DylRey18 <111478803+DylRey18@users.noreply.github.com>
2025-04-25 16:12:56 +0700

    added right sidebar

--- Frontend/twitter-clone/src/components/pagesLayout/SidebarRight.js
+++ Frontend/twitter-clone/src/components/pagesLayout/SidebarRight.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import "../../assets/styles/SidebarRight.css";
+import {
+  TwitterTweetEmbed
+} from "react-twitter-embed"
+import { CiSearch } from "react-icons/ci";
 
 function SidebarRight() {
   return (
-    <div className='sidebar-right'>SidebarRight</div>
+    <div className='sidebar-right'>
+      
+      {/* Search bar */}
+      <div className='sidebar-right-input'>
+        <CiSearch className='sidebar-right-searchIcon'/>
+        <input placeholder="Search" type="text" />
+      </div>
+
+      {/* What's Happening */}
+      <div className='sidebar-right-container'>
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId='1915572372842963181'/>
+
+      </div>
+
+    </div>
   )
 }
 
