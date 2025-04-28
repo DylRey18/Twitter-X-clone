d8fb458ee84ab0f453b4e95711301371b3f51c33
DylRey18 <111478803+DylRey18@users.noreply.github.com>
2025-04-28 16:27:37 +0700

    update

--- Frontend/twitter-clone/src/components/pagesFunction/Home.js
+++ Frontend/twitter-clone/src/components/pagesFunction/Home.js
@@ -3,7 +3,8 @@ import "../../assets/styles/Home.css"
 import TweetboxForm from '../form/TweetboxForm'
 import Post from '../card/Post.js'
 import dyl from '../../assets/image/Dylpp.png'
-
+import anila from '../../assets/image/Anila.png'
+import reddit from '../../assets/image/reddit.png'
 
 function Home() {
   return (
@@ -20,8 +21,8 @@ function Home() {
 
           {/* post */}
           <Post displayName="Dylan Theodore" username="dylclone" verified={true} text={"This is from Dylan"} avatar={dyl} image="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGJycGVjODRycWlrdXQ3aWp0dHU3Z3FjdmIzbDhneHE5bjU2N3RwNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kYNVwkyB3jkauFJrZA/giphy.gif"/>
-          <Post displayName="iAmABot" username="bottwitter" verified={false} text={"This is from Dylan"} avatar={dyl}/>
-          <Post displayName="Dylan Theodore" username="dylclone" verified={true} text={"This is from Dylan"} avatar={dyl}/>
+          <Post displayName="iAmABot" username="bottwitter" verified={false} text={"Happy Birthday!"} avatar={reddit} image="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXozNnY5anoxd2pnYzl2azRhNm4wMnBlYTdicjZ3cnNxOG8zc2xiaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/h3WH1rqyW2bmfOVqSi/giphy.gif"/>
+          <Post displayName="Anila la" username="anilul" verified={false} text={"Let's Dance!!!"} avatar={anila} image="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmFicGs0YnVieGkwYmlxbmYybGw3NXBpYnQxdTJzNzVqMzFxN3V6aSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kyLYXonQYYfwYDIeZl/giphy.gif"/>
                    
         
       </div>
